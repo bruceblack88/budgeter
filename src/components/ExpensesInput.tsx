ebe8c2108332a4789339b532495964c4dbde4cd0
Bruce Black <bruceblack88@gmail.com>
2023-10-11 21:14:01 -0500

    Add MUI Add MUI components Add Expense Category Dropdown

--- src/components/ExpensesInput.tsx
+++ src/components/ExpensesInput.tsx
@@ -1,4 +1,5 @@
 import {ChangeEvent} from "react";
+import {Box, FormControl, FormLabel, Input, InputLabel, NativeSelect, Typography} from "@mui/material";
 
 export interface ExpensesCategories {
     housing: number;
@@ -40,33 +41,61 @@ function ExpensesInput({expenses, handleInputChange}: ExpensesInputProps) {
     }
     return (
         <>
-            <div>
-                <h2>Expenses:</h2>
-                <div>
-                    <label htmlFor={expenseLabel.housing.toLowerCase()}>
+            <Box>
+                <Typography variant={"h2"}>Expenses:</Typography>
+                <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel variant="standard" htmlFor="expense-category">
+                            Choose Expense Category
+                        </InputLabel>
+                        <NativeSelect
+                            defaultValue={expenseLabel.housing}
+                            inputProps={{
+                                name: 'expense-category',
+                                id: 'expense-category',
+                            }}
+                        >
+                            <option value={expenseLabel.housing}>{expenseLabel.housing}</option>
+                            <option value={expenseLabel.utilities}>{expenseLabel.utilities}</option>
+                            <option value={expenseLabel.transportation}>{expenseLabel.transportation}</option>
+                            <option value={expenseLabel.groceries}>{expenseLabel.groceries}</option>
+                            <option value={expenseLabel.dining}>{expenseLabel.dining}</option>
+                            <option value={expenseLabel.healthcare}>{expenseLabel.healthcare}</option>
+                            <option value={expenseLabel.insurance}>{expenseLabel.insurance}</option>
+                            <option value={expenseLabel.debt}>{expenseLabel.debt}</option>
+                            <option value={expenseLabel.entertainment}>{expenseLabel.entertainment}</option>
+                            <option value={expenseLabel.savings}>{expenseLabel.savings}</option>
+                            <option value={expenseLabel.education}>{expenseLabel.education}</option>
+                            <option value={expenseLabel.childcare}>{expenseLabel.childcare}</option>
+                            <option value={expenseLabel.miscellaneous}>{expenseLabel.miscellaneous}</option>
+                        </NativeSelect>
+                    </FormControl>
+                </Box>
+                <Box>
+                    <FormLabel htmlFor={expenseLabel.housing.toLowerCase()}>
                         {expenseLabel.housing}
-                    </label>
-                    <input
+                    </FormLabel>
+                    <Input
                         type="number"
                         name={expenseLabel.housing.toLowerCase()}
                         id={expenseLabel.housing.toLowerCase()}
                         placeholder="0.00"
                         value={expenses.housing || ''}
                         onChange={handleInputChange}/>
-                </div>
-                <div>
-                    <label htmlFor={expenseLabel.utilities.toLowerCase()}>
+                </Box>
+                <Box>
+                    <InputLabel htmlFor={expenseLabel.utilities.toLowerCase()}>
                         {expenseLabel.utilities}
-                    </label>
-                    <input
+                    </InputLabel>
+                    <Input
                         type="number"
                         name={expenseLabel.utilities.toLowerCase()}
                         id={expenseLabel.utilities.toLowerCase()}
                         placeholder="0.00"
                         value={expenses.utilities || ''}
                         onChange={handleInputChange}/>
-                </div>
-            </div>
+                </Box>
+            </Box>
         </>
     );
 }
