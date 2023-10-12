ca3738cfe62cb619c857199e8a886b969306909b
Bruce Black <bruceblack88@gmail.com>
2023-10-11 20:53:19 -0500

    Change component property literals to variables

--- src/components/ExpensesInput.tsx
+++ src/components/ExpensesInput.tsx
@@ -17,7 +17,6 @@ export interface ExpensesCategories {
 }
 
 
-
 interface ExpensesInputProps {
     expenses: Partial<ExpensesCategories>;
     handleInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -25,53 +24,50 @@ interface ExpensesInputProps {
 
 function ExpensesInput({expenses, handleInputChange}: ExpensesInputProps) {
     const expenseLabel = {
-    housing: "Housing",
-    utilities: "Utilities",
-    transportation: "Transportation",
-    groceries: "Groceries",
-    dining: "Dining",
-    healthcare: "Healthcare",
-    insurance: "Insurance",
-    debt: "Debt",
-    entertainment: "Entertainment",
-    savings: "Savings",
-    education: "Education",
-    childcare: "Childcare",
-    miscellaneous: "Miscellaneous"
-}
+        housing: "Housing",
+        utilities: "Utilities",
+        transportation: "Transportation",
+        groceries: "Groceries",
+        dining: "Dining",
+        healthcare: "Healthcare",
+        insurance: "Insurance",
+        debt: "Debt",
+        entertainment: "Entertainment",
+        savings: "Savings",
+        education: "Education",
+        childcare: "Childcare",
+        miscellaneous: "Miscellaneous"
+    }
     return (
-        <div>
-            <h2>Expenses:</h2>
+        <>
             <div>
-                <label htmlFor="housing">
-                    {expenseLabel.housing}
-                </label>
-                <input
-                    type="number"
-                    name="housing"
-                    id="housing"
-                    placeholder="0.00"
-                    value={expenses.housing || ''}
-                    onChange={handleInputChange}
-                    className="w-full p-2 border rounded"
-                />
+                <h2>Expenses:</h2>
+                <div>
+                    <label htmlFor={expenseLabel.housing.toLowerCase()}>
+                        {expenseLabel.housing}
+                    </label>
+                    <input
+                        type="number"
+                        name={expenseLabel.housing.toLowerCase()}
+                        id={expenseLabel.housing.toLowerCase()}
+                        placeholder="0.00"
+                        value={expenses.housing || ''}
+                        onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label htmlFor={expenseLabel.utilities.toLowerCase()}>
+                        {expenseLabel.utilities}
+                    </label>
+                    <input
+                        type="number"
+                        name={expenseLabel.utilities.toLowerCase()}
+                        id={expenseLabel.utilities.toLowerCase()}
+                        placeholder="0.00"
+                        value={expenses.utilities || ''}
+                        onChange={handleInputChange}/>
+                </div>
             </div>
-
-            <div>
-                <label htmlFor="utilities">
-                    {expenseLabel.utilities}
-                </label>
-                <input
-                    type="number"
-                    name="utilities"
-                    id="utilities"
-                    placeholder="0.00"
-                    value={expenses.utilities || ''}
-                    onChange={handleInputChange}
-                    className="w-full p-2 border rounded"
-                />
-            </div>
-        </div>
+        </>
     );
 }
 
