955fefaf2a90521cdf273150eca3276798913670
Bruce Black <bruceblack88@gmail.com>
2023-10-14 14:11:01 -0500

    Add form to a card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {ChangeEvent, FormEvent, useState} from 'react';
 import ExpensesInput from "./components/ExpensesInput";
-import {Box, Button, FormGroup, Typography} from "@mui/material";
+import {Box, Button, Card, CardContent, FormGroup, Step, StepLabel, Stepper, Typography} from "@mui/material";
 import IncomeInput from "./components/IncomeInput.tsx";
 import SavingsGoalInput from "./components/SavingsGoalInput.tsx";
 
@@ -34,17 +34,59 @@ function App() {
         console.log(formData);
     };
 
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = ['Income', 'Expenses', 'Savings Goal'];
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
     return (
         <Box>
             <Typography variant={"h1"}>BUDGETEER</Typography>
-            <form onSubmit={handleSubmit}>
-                <FormGroup>
-                    <IncomeInput income={formData} handleInputChange={handleInputChange} />
-                    <ExpensesInput handleInputChange={handleInputChange} expenses={formData}/>
-                    <SavingsGoalInput savings={formData} handleInputChange={handleInputChange}/>
-                    <Button type="submit">Save</Button>
-                </FormGroup>
-            </form>
+            <Card variant={"outlined"}>
+                <CardContent>
+                    <Stepper activeStep={activeStep}>
+                        {steps.map((label) => (
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    <form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            {activeStep === 0 && <IncomeInput income={formData} handleInputChange={handleInputChange}/>}
+                            {activeStep === 1 &&
+                                <ExpensesInput handleInputChange={handleInputChange} expenses={formData}/>}
+                            {activeStep === 2 &&
+                                <SavingsGoalInput savings={formData} handleInputChange={handleInputChange}/>}
+
+                            {activeStep === steps.length - 1 ? (
+                                <Button type="submit">Save</Button>
+                            ) : (
+                                <>
+                                    <Button onClick={handleBack} disabled={activeStep === 0}>Back</Button>
+                                    <Button variant="contained" color="primary" onClick={handleNext}>Next</Button>
+                                </>
+                            )}
+
+                            {activeStep !== steps.length && (
+                                <Button onClick={handleReset}>
+                                    Reset
+                                </Button>
+                            )}
+                        </FormGroup>
+                    </form>
+                </CardContent>
+            </Card>
         </Box>
     );
 }
